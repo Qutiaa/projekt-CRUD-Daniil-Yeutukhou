0dbde080df7da7d7394693d32e7591857146c55b
Qutiaa <daniilsegeevich@gmail.com>
2025-10-23 15:08:31 +0200

    Dodano db.create_all() w celu tworzenia tabel podczas renderowania

--- flask_crud_shop.py
+++ flask_crud_shop.py
@@ -7,6 +7,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///products.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+with app.app_context():
+    db.create_all()
 
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
